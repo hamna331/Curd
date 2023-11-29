d9d6941a7bc520f5ac3409dcf282018c69803efe
ayanadankhan <ayanadankhan@gmail.com>
2023-11-29 15:41:56 +0500

    shop list

---
+++ src/Shop/shopList.js
@@ -0,0 +1,204 @@
+import img1 from '../images/Balancia Electronic weight .jpg'
+import img2 from '../images/Header-01.jpg'
+import img3 from '../images/anti-agging-cream.jpg'
+import img4 from '../images/anti-wrinkle.webp'
+import img5 from '../images/bag.jph.webp'
+import img6 from '../images/cold-cream.jpg'
+import img7 from '../images/consumer-electronic.jpg'
+import img8 from '../images/conventional.jpg'
+import img9 from '../images/electronic.jpg'
+import img10 from '../images/engine.jpg'
+import img11 from '../images/foot-care.jpg'
+import img12 from '../images/glow-up.jpg'
+import img13 from '../images/hardware.jpg'
+import img14 from '../images/herbal-cream.jpg'
+import img15 from '../images/pit-cream.jpg'
+import img16 from '../images/recycling.jpg'
+import img17 from '../images/vanishing.jpg'
+import img18 from '../images/white-glow.jpg'
+import img19 from '../images/world-skill.jpg'
+import img20 from '../images/img1.jpg'
+import img21 from '../images/img2.jpg'
+import img22 from '../images/img3.jpg'
+import img23 from '../images/img4.jpg'
+import img24 from '../images/img5.jpg'
+import img25 from '../images/img6.jpg'
+import img26 from '../images/img7.jpg'
+import img27 from '../images/img8.jpg'
+
+const Sdata =[
+    {
+        imgsrc:img1,
+        title : "Balancia Electronic weight",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img2,
+        title : "Header",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img3,
+        title : "anti-agging-cream",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img4,
+        title : "anti-wrinkle",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img5,
+        title : "bag",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img6,
+        title : "cold-cream",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img7,
+        title : "consumer-electronic",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img8,
+        title : "conventional",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img9,
+        title : "electronic",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img10,
+        title : "engine",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img11,
+        title : "foot-care",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img12,
+        title : "glow-up",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img13,
+        title : "hardware",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img14,
+        title : "herbal-cream",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img15,
+        title : "pit-cream",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img16,
+        title : "recycling",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img17,
+        title : "vanishing",
+        category:"man",
+        btn : "view"
+    },
+    {
+        imgsrc:img18,
+        title : "white-glow",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img19,
+        title : "world-skill",
+        category:"woman",
+        btn : "view"
+    },
+    {
+        imgsrc:img20,
+        title : "baby-toye",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img21,
+        title : "baby-toye",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img22,
+        title : "baby-toye",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img23,
+        title : "baby-toye",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img24,
+        title : "baby-toye",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img25,
+        title : "baby-toye",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img26,
+        title : "bay-toys",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img26,
+        title : "bay-toys",
+        category:"kid",
+        btn : "view"
+    },
+    {
+        imgsrc:img27,
+        title : "bay-toys",
+        category:"kid",
+        btn : "view"
+    },
+    
+]
+
+
+
+
+export  default Sdata;
