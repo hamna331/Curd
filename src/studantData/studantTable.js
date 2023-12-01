4777b628eb97213313dc98722dd12df8a9c790e3
hamna331 <hmtariq331@gmail>
2023-12-01 15:35:50 +0500

    comit

--- src/studantData/studantTable.js
+++ src/studantData/studantTable.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate,useLocation } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { AiFillDelete } from "react-icons/ai";
+import { GrView } from "react-icons/gr";
+import { FaEdit } from "react-icons/fa";
 
 const TableComponent = () => {
-  const [formData, setFormData] = useState([])
+  const [formData, setFormData] = useState([]);
   const navigate = useNavigate();
   const location = useLocation();
-  const searchQuery = new URLSearchParams(location.search).get('search');
-
+  const searchQuery = new URLSearchParams(location.search).get("search");
 
   useEffect(() => {
     console.log("Search query in TableComponent:", searchQuery);
- 
+
     const savedFormData = JSON.parse(localStorage.getItem("formData")) || [];
-    console.log("==========>",savedFormData);
+    console.log("==========>", savedFormData);
     setFormData(savedFormData);
   }, [setFormData]);
 
   if (!Array.isArray(formData)) {
-    console.error('formData is not an array:', formData);
+    console.error("formData is not an array:", formData);
     return null;
   }
 
@@ -35,15 +37,15 @@ const TableComponent = () => {
   const handleView = (index) => {
     navigate(`/student-card/${index}`);
   };
-  
-  
 
   const filteredData = formData.filter((data) => filterNames(data));
-
+  console.log("hhhhhh",filteredData);
 
   function filterNames(data) {
-    const currentSearchQuery = new URLSearchParams(location.search).get('search');
-  
+    const currentSearchQuery = new URLSearchParams(location.search).get(
+      "search"
+    );
+
     if (currentSearchQuery) {
       const lowercaseQuery = currentSearchQuery.toLowerCase();
       return (
@@ -54,17 +56,18 @@ const TableComponent = () => {
         // Add more fields as needed
       );
     }
-  
+
     return true; // If no search query, show all entries
   }
-  
 
   return (
-    <div className="container mt-5">
-      <h2 className="text-center mb-4">Student Data Table</h2>
+    <div className="container-fluid mt-5 ">
+      <h2 className="text-center mb-4 fw-bold">Student Data Table</h2>
       <table className="table table-bordered">
         <thead>
           <tr>
+            {/* <th className="text-center">sr.</th> */}
+            <th className="text-center">profile</th>
             <th className="text-center">First Name</th>
             <th className="text-center">Last Name</th>
             <th className="text-center">Email</th>
@@ -73,54 +76,53 @@ const TableComponent = () => {
             <th className="text-center">Phone</th>
             <th className="text-center">Birthdate</th>
             <th className="text-center">Gender</th>
+            <th className="text-center">country</th>
             <th className="text-center">Actions</th>
           </tr>
         </thead>
         <tbody>
-          {filteredData
-         
-            .map((data, index) => (
-              <tr key={index}>
-                <td className="text-center">{data.fName}</td>
-                <td className="text-center">{data.lName}</td>
-                <td className="text-center">{data.email}</td>
-                <td className="text-center">{data.class}</td>
-                <td className="text-center">{data.grade}</td>
-                <td className="text-center">{data.phone}</td>
-                <td className="text-center">
-                  {data.birthdate instanceof Date
-                    ? data.birthdate.toLocaleDateString()
-                    : data.birthdate}
-                </td>
-                <td className="text-center">{data.gender}</td>
-                <td className="row text-center">
-                {/* <Link
-                  className="btn btn-primary me-2"
-                  onClick={() => handleView(index)} // Call handleView on button click
+          {filteredData.map((data, index) => (
+            <tr key={index}>
+              {/* <td className="text-center">{data.index+1}</td> */}
+              <td className="text-center"> <img src={data.profilePicture} alt="Profile" style={{ width: '100px', height: '100px' }} /></td>
+              <td className="text-center">{data.fName}</td>
+              <td className="text-center">{data.lName}</td>
+              <td className="text-center">{data.email}</td>
+              <td className="text-center">{data.class}</td>
+              <td className="text-center">{data.grade}</td>
+              <td className="text-center">{data.phone}</td>
+              <td className="text-center">
+                {data.birthdate instanceof Date
+                  ? data.birthdate.toLocaleDateString()
+                  : data.birthdate}
+              </td>
+              <td className="text-center">{data.gender}</td>
+              <td className="text-center">{data.country}</td>
+
+              <td>
+                <button
+                  className="btn btn-warning m-1"
+                  onClick={() => handleView(index)}
                 >
-                  View
-                </Link> */}
-                 <button
-                    className="btn btn-warning"
-                    onClick={() => handleView(index)}
-                  >
-                    View
-                  </button>
-                  <button
-                    className="btn btn-danger me-2"
-                    onClick={() => deleteEntry(index)}
-                  >
-                    Delete
-                  </button>
-                  <button
-                    className="btn btn-warning"
-                    onClick={() => handleEdit(index)}
-                  >
-                    Edit
-                  </button>
-                </td>
-              </tr>
-            ))}
+                 <i className="them-color "> <GrView /></i>
+                </button>
+                <button
+                  className="btn btn-danger m-1"
+                  onClick={() => deleteEntry(index)}
+                >
+                  <i className="them-color ">
+                    <AiFillDelete />
+                  </i>
+                </button>
+                <button
+                  className="btn btn-warning m-1"
+                  onClick={() => handleEdit(index)}
+                >
+                  <i className="them-color"><FaEdit/></i>
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
